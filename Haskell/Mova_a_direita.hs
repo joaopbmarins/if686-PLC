3313bd72a130068f8d97b519b4299c8f732a1de5
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2025-05-20 16:27:44 -0300

    Create Mova_a_direita.hs

---
+++ Haskell/Mova_a_direita.hs
@@ -0,0 +1,11 @@
+parseInput str = let [n, s] = words str
+                 in (read n, s)
+main :: IO()
+main = interact $ uncurry paraDireita . parseInput
+
+addEspacos :: Int -> String
+addEspacos n | n == 0 = ""
+             | otherwise = addEspacos (n-1) ++ " "
+
+paraDireita :: Int -> String -> String
+paraDireita n str = addEspacos(n) ++ str
