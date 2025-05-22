019c091113ce60b86336fc5afe6e000476c93870
João Pedro Marins <136388066+joaopbmarins@users.noreply.github.com>
2025-05-21 23:41:47 -0300

    corrigindo indentação

--- Haskell/aula4.hs
+++ Haskell/aula4.hs
@@ -19,12 +19,12 @@ name :: Person -> Name
 name (n,a,p) = n
 
 oneRoot :: Float -> Float -> Float -> Float
-oneRoot a b c =-b/(2.0*a)
+oneRoot a b c = -b/(2.0*a)
 
 twoRoots :: Float -> Float -> Float -> (Float, Float)
 twoRoots a b c = (d-e, d+e)
     where
-        d =-b/(2.0*a)
+        d = -b/(2.0*a)
         e = sqrt (b^2-4.0*a*c)/(2.0*a)
 
 roots :: Float -> Float -> Float -> String
